6912d8f3aa7fa89a3ff2067acd63593f49fc4aaa
innadev1 <innad2201@gmail.com>
2017-10-17 10:39:14 +0300

    new

--- index.php
+++ index.php
@@ -534,7 +534,7 @@
 							<div class="cell1 background1"><div id="trim_2"></div></div>
 							<div class="cell2 background2">
 							<div class="text_block">
-							<h3>Тур в Австрию</h3>
+							<h3>Тур в Марокко</h3>
 							<div class="text_border"></div>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
 							<p class="signature">-Martin</p>
